74fb2ede3b013d122634f38fc3933cbbbea90d09
Pedro Aaron <p.luis.aaron@gmail.com>
2020-04-10 11:53:38 -0500

    Add Exercise 21
    Add Exercise 21

---
+++ Exercise 21/ex21.py
@@ -0,0 +1,37 @@
+def add(a: object, b: object) -> object:
+    print(f"ADDING {a} + {b}")
+    return a + b
+
+
+def subtract(a, b):
+    print(f"SUBTRACTING {a} - {b}")
+    return a - b
+
+
+def multiply(a, b):
+    print(f"MULTIPLY {a} * {b}")
+    return a * b
+
+
+def divide(a, b):
+    print(f"DIVING {a} / {b}")
+    return a / b
+
+
+print(f"Let's do some math just funcions!")
+
+age = add(30, 5)
+height = subtract(78, 4)
+weight = multiply(90, 2)
+iq = divide(100, 2)
+
+print(f"Age: {age}, Height: {height}, Weight: {weight}, IQ: {iq}")
+
+# A puzzle for the extra credit, type it in anyway.
+
+print("Here is a puzzle.")
+
+what = add(age, subtract(height, multiply(weight, divide(iq, 2))))
+
+print("That becomes:", what, "Can you do it by hand?")
+
